69474838a8cdbe8c8530e00c7a648a6c28474cc6
Виктория Р <134516778+Victoriya-R@users.noreply.github.com>
2023-09-02 15:57:38 +0300

    Update loginautotest.js
    изменила url

--- cypress/integration/Test-qa/loginautotest.js
+++ cypress/integration/Test-qa/loginautotest.js
@@ -2,10 +2,10 @@
 describe('Проверка формы логина', function () {
 
     it('Позитивный кейс:верные логин и пароль', function () {
-         cy.visit('https://login.qa.studio/');
-         cy.get('#mail').type('german@dolnikov.ru'); 
+         cy.visit('https://login.qa.com/');
+         cy.get('#mail').type('валиднная почта'); 
          cy.get('#loginButton').should('be.disabled');
-         cy.get('#pass').type('iLoveqastudio1');
+         cy.get('#pass').type('валидный пароль');
          cy.get('#loginButton').should('not.be.disabled');
          cy.get('#loginButton').click();
          cy.contains('Авторизация прошла успешно');
@@ -15,9 +15,9 @@ describe('Проверка формы логина', function () {
 
      it('Негативный кейс:верные логин и неверный пароль', function () {
         cy.reload();
-        cy.get('#mail').type('german@dolnikov.ru'); 
+        cy.get('#mail').type('валидная почта'); 
         cy.get('#loginButton').should('be.disabled');
-        cy.get('#pass').type('iLoveqastudio2');
+        cy.get('#pass').type('неверный пароль');
         cy.get('#loginButton').should('not.be.disabled');
         cy.get('#loginButton').click();
         cy.contains('Такого логина или пароля нет');
