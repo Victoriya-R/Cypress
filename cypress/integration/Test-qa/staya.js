2bdfe956333bd547294554bf2362a19220305af8
Виктория Р <134516778+Victoriya-R@users.noreply.github.com>
2023-09-02 16:04:06 +0300

    Update qaengineer.js

--- cypress/integration/Test-qa/staya.js
+++ cypress/integration/Test-qa/staya.js
@@ -1,9 +1,10 @@
+//Название сайта изменено из-за политики конфиденциальности
 
-describe('Проверка покупки на сайте staya.dog (этот тест падает.т.к на сайте постоянные изменения)', function () {
+describe('Проверка быстрой покупки на сайте Qaengineer.com ', function () {
     
     it('Проваливаюсь в категорию поводки и ищу совпадение с названием Heatwave', function () {
         
-        cy.visit('https://staya.dog/').pageLoadTimeout;
+        cy.visit('https://Qaengineer.com/').pageLoadTimeout;
         cy.get('[href="/catalog/harnesses"] > .category-link__canvas-wrapper > .category-link__canvas').click();
         cy.get(':nth-child(1) > .SProductCard__galleryWrapper > .CurtainGallery').click();
         cy.get('.SSpoiler__container > :nth-child(3)').click();
